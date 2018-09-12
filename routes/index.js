39f01104599f6d54d8c1fd8e91831b75d9e4b44a
herengadrien <39774261+herengadrien@users.noreply.github.com>
2018-09-12 19:34:14 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+const panier= [{
+  nomProduit:'',
+  prixProduit:'',
+  dateReservation:'',
+  idUser:''
+}]
+
+
+const user = [{
+  email: 'adri@test',
+  password: '1234',
+  date: 'kkkkk'
+}, {
+  email: 'emma@test',
+  password: '1234',
+  date: ''
+}]
+// user
+router.get('/user', (req, res) => {
+  res.json(user)
+})
+router.post('/user', (req, res) => {
+  user.push({
+  email: req.body.email,
+  password: req.body.password,
+  date: req.body.date
+  })
+  res.send('OK')
+})
+// panier
+router.get('/panier', (req, res) => {
+  res.json(panier)
+})
+router.post('/panier', (req, res) => {
+  panier.push({
+  nomProduit: req.body.nomProduit,
+  prixProduit: req.body.prixProduit,
+  dateReservation: req.body.dateReservation,
+  idUser: req.body.idUser
+
+  })
+  res.send('OKok')
+})
+
+//usless
+/*router.post('/connect', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (email === 'adri@adri' && password === 'aaaa') {
+    // l'utilisateur est connecté
+  } else {
+
+    res.status(400).send('Bouuuh mauvais mdp')
+
+  }
+})*/
+module.exports = router
